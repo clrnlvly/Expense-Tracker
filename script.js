f9c943229badf6486ed9457530db04d1648a9eba
clrnlvly <142940454+clrnlvly@users.noreply.github.com>
2025-06-11 07:55:45 +0800

    building
    html and css

---
+++ script.js
@@ -0,0 +1,8 @@
+const balanceEl = document.getElementById("balance");
+const incomeAmountEl = document.getElementById("income-amount");
+const expenseAmountEl = document.getElementById("expense-amount");
+const transactionListEl = document.getElementById("transaction-list");
+const transactionFormEl = document.getElementById("transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
+
